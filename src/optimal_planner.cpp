f024ccb7d84933f4932f4a323ea2438263c47164
Daiki Maekawa <d.maekawa@rapyuta-robotics.com>
2016-09-28 18:44:07 +0900

    optimize lookahead distance

--- src/optimal_planner.cpp
+++ src/optimal_planner.cpp
@@ -836,7 +836,7 @@ bool TebOptimalPlanner::getSetpointCommand(const PoseSE2 &robot_pose, rr_base_ca
     return false;
   }
     
-  const double lookahead_dist = 0.5;
+  const double lookahead_dist = 0.6;
   ROS_INFO_STREAM("teb size = " << teb_.sizePoses());
   
   double sum_dt = 0.0;
